485419f4824e657b58eae607410e8a59ba71c304
Jessie Wong <jessie14@student.ubc.ca>
2022-01-18 23:40:30 -0800

    Initial commit

---
+++ R/plot_geographical.R
@@ -0,0 +1,13 @@
+#' Plot Cloropleth Map of Covid Data
+#'
+#' @param covid_df dataframe containing covid data to plot
+#' @param metric column in the dataframe to plot
+#'
+#' @return plot object of cloropleth map
+#' @export
+#'
+#' @examples
+#' plot_geographical(covid_df,'cases')
+plot_geographical <- function(covid_df, metric) {
+
+}
